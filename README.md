03618743403888a045924da41c95bac9b7ae2da7
wagiminator <wagiminator@web.de>
2021-09-26 09:29:13 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# ATtiny13-TinySat
-Solar Powered Blinken Lights
+# TinySat - Solar Powered Blinken Lights
+TinySat collects the energy from up to two solar panels (max 6V) and stores it in a 1 Farad super capacitor. The ATtiny13A uses this stored energy to make two LEDs blink - even when the sun is not shining.
+
+- Design Files (EasyEDA): https://easyeda.com/wagiminator/attiny13-tinysat
+
+![TinySat_pic1.jpg](https://raw.githubusercontent.com/wagiminator/ATtiny13-TinySat/main/documentation/TinySat_pic1.jpg)
+
+# Hardware
+![TinySat_wiring.png](https://raw.githubusercontent.com/wagiminator/ATtiny13-TinySat/main/documentation/TinySat_wiring.png)
+
+# Compiling and Uploading the Firmware
+Since there is no ICSP header on the board, you have to program the ATtiny before putting it into the IC socket. The [AVR Programmer Adapter](https://github.com/wagiminator/AVR-Programmer/tree/master/AVR_Programmer_Adapter) can help with this.
+
+## If using the Arduino IDE
+- Make sure you have installed [MicroCore](https://github.com/MCUdude/MicroCore).
+- Go to **Tools -> Board -> MicroCore** and select **ATtiny13**.
+- Go to **Tools** and choose the following board options:
+  - **Clock:**  128 kHz internal osc.
+  - **BOD:**    disabled
+  - **Timing:** Micros disabled
+- Connect your programmer to your PC and to the ATtiny.
+- Go to **Tools -> Programmer** and select your ISP programmer (e.g. [USBasp](https://aliexpress.com/wholesale?SearchText=usbasp)).
+- Go to **Tools -> Burn Bootloader** to burn the fuses.
+- Open the TinySat sketch and click **Upload**.
+
+## If using the precompiled hex-file
+- Make sure you have installed [avrdude](https://learn.adafruit.com/usbtinyisp/avrdude).
+- Connect your programmer to your PC and to the ATtiny.
+- Open a terminal.
+- Navigate to the folder with the hex-file.
+- Execute the following command (if necessary replace "usbasp" with the programmer you use):
+  ```
+  avrdude -c usbasp -p t13 -U lfuse:w:0x3b:m -U hfuse:w:0xff:m -U flash:w:tinysat.hex
+  ```
+
+## If using the makefile (Linux/Mac)
+- Make sure you have installed [avr-gcc toolchain and avrdude](http://maxembedded.com/2015/06/setting-up-avr-gcc-toolchain-on-linux-and-mac-os-x/).
+- Connect your programmer to your PC and to the ATtiny.
+- Open the makefile and change the programmer if you are not using usbasp.
+- Open a terminal.
+- Navigate to the folder with the makefile and the sketch.
+- Run "make install" to compile, burn the fuses and upload the firmware.
+
+# License
+![license.png](https://i.creativecommons.org/l/by-sa/3.0/88x31.png)
+
+This work is licensed under Creative Commons Attribution-ShareAlike 3.0 Unported License. 
+(http://creativecommons.org/licenses/by-sa/3.0/)
---
+++ software/TinySat.ino
@@ -0,0 +1,119 @@
+// TinySat - Solar Powered Blinken Lights
+//
+//                     +-\/-+
+//   --- A0 (D5) PB5  1|°   |8  Vcc
+//   --- A3 (D3) PB3  2|    |7  PB2 (D2) A1 --- 
+//   --- A2 (D4) PB4  3|    |6  PB1 (D1) ------ LED1 PWM
+//               GND  4|    |5  PB0 (D0) ------ LED0 PWM
+//                     +----+    
+//
+// Controller:  ATtiny13A
+// Core:        MicroCore (https://github.com/MCUdude/MicroCore)
+// Clockspeed:  128 kHz internal
+// BOD:         disabled (to save energy)
+// Timing:      Micros disabled
+// Leave the rest on default settings. Don't forget to "Burn bootloader"!
+// No Arduino core functions or libraries are used. Use the makefile if 
+// you want to compile without Arduino IDE.
+//
+// 2021 by Stefan Wagner 
+// Project Files (EasyEDA): https://easyeda.com/wagiminator
+// Project Files (Github):  https://github.com/wagiminator
+// License: http://creativecommons.org/licenses/by-sa/3.0/
+
+
+// Libraries
+#include <avr/io.h>             // for gpio
+#include <avr/sleep.h>          // for the sleep modes
+#include <avr/interrupt.h>      // for the interrupts
+#include <avr/power.h>          // for the power control
+#include <avr/wdt.h>            // for the watchdog timer
+
+// Pin definitions
+#define LED0          PB0       // pin for LED 0
+#define LED1          PB1       // pin for LED 1
+
+// -----------------------------------------------------------------------------
+// Watchdog Timer Implementation
+// -----------------------------------------------------------------------------
+
+// Reset watchdog timer
+void resetWatchdog(void) {
+  cli();                                  // timed sequence coming up
+  wdt_reset();                            // reset watchdog
+  MCUSR = 0;                              // clear various "reset" flags
+  WDTCR = (1<<WDCE)|(1<<WDE)|(1<<WDTIF);  // allow changes, clear interrupt
+  WDTCR = (1<<WDTIE)|(1<<WDP3);           // set WDT interval 4 seconds
+  sei();                                  // interrupts are required now
+}
+
+// Watchdog interrupt service routine
+ISR (WDT_vect) {
+  wdt_disable();                          // disable watchdog
+}
+
+// -----------------------------------------------------------------------------
+// Main Function
+// -----------------------------------------------------------------------------
+
+// Main function
+int main(void) {
+  // Reset watchdog timer
+  resetWatchdog();                        // do this first in case WDT fires
+  
+  // PWM setup
+  TCCR0A = (1<<COM0A1) | (1<<COM0B1)      // clear OC0A/OC0B on compare match, set at TOP
+         | (1<<WGM01)  | (1<<WGM00);      // fast PWM 0x00 - 0xff
+  TCCR0B = (1<<CS00);                     // start timer without prescaler
+  TIMSK0 = (1<<TOIE0);                    // timer/counter0 overflow interrupt enable
+
+  // Setup pins
+  DDRB   = (1<<LED0) | (1<<LED1);         // LED pins as output
+  
+  // Disable unused peripherals to save power
+  ADCSRA = 0;                             // disable ADC
+  ACSR   = (1<<ACD);                      // disable analog comperator
+  PRR    = (1<<PRADC);                    // shut down ADC
+  DIDR0  = 0x1F;                          // disable digital intput buffer
+  
+  // Main loop
+  while(1) {
+    // Fade in/out LED 0
+    uint8_t i = 0;
+    set_sleep_mode (SLEEP_MODE_IDLE);
+    while(++i) {
+      OCR0A = i;
+      sleep_mode();
+    }
+    while(--i) {
+      OCR0A = i;
+      sleep_mode();
+    }
+    
+    // Sleep for 4 seconds
+    OCR0A = 0;
+    set_sleep_mode (SLEEP_MODE_PWR_DOWN);
+    resetWatchdog();
+    sleep_mode();
+    
+    // Fade in/out LED 1
+    set_sleep_mode (SLEEP_MODE_IDLE);
+    while(++i) {
+      OCR0B = i;
+      sleep_mode();
+    }
+    while(--i) {
+      OCR0B = i;
+      sleep_mode();
+    }
+    
+    // Sleep for 4 seconds
+    OCR0B = 0;
+    set_sleep_mode (SLEEP_MODE_PWR_DOWN);
+    resetWatchdog();
+    sleep_mode(); 
+  }
+}
+
+// Timer0 overflow interrupt service routine
+EMPTY_INTERRUPT (TIM0_OVF_vect);    // nothing to be done here, just wake up from sleep
