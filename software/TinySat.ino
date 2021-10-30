d5e9e6803b23098819ed34372459be4dd482e145
wagiminator <wagiminator@web.de>
2021-10-30 19:30:15 +0200

    Editorial changes

--- software/TinySat.ino
+++ software/TinySat.ino
@@ -1,41 +1,54 @@
-// TinySat - Solar Powered Blinken Lights
+// ===================================================================================
+// Project:   TinySat - Solar Powered Blinken Lights based on ATtiny13A
+// Version:   v1.0
+// Year:      2021
+// Author:    Stefan Wagner
+// Github:    https://github.com/wagiminator
+// EasyEDA:   https://easyeda.com/wagiminator
+// License:   http://creativecommons.org/licenses/by-sa/3.0/
+// ===================================================================================
 //
-//                     +-\/-+
-//   --- A0 (D5) PB5  1|°   |8  Vcc
-//   --- A3 (D3) PB3  2|    |7  PB2 (D2) A1 --- 
-//   --- A2 (D4) PB4  3|    |6  PB1 (D1) ------ LED1 PWM
-//               GND  4|    |5  PB0 (D0) ------ LED0 PWM
-//                     +----+    
+// Wiring:
+// -------
+//                         +-\/-+
+//      --- RST ADC0 PB5  1|°   |8  Vcc
+//      ------- ADC3 PB3  2|    |7  PB2 ADC1 -------- 
+//      ------- ADC2 PB4  3|    |6  PB1 AIN1 OC0B --- LED1 PWM
+//                   GND  4|    |5  PB0 AIN0 OC0A --- LED0 PWM
+//                         +----+
 //
+// Compilation Settings:
+// ---------------------
 // Controller:  ATtiny13A
 // Core:        MicroCore (https://github.com/MCUdude/MicroCore)
 // Clockspeed:  128 kHz internal
-// BOD:         disabled (to save energy)
+// BOD:         BOD disabled
 // Timing:      Micros disabled
+//
 // Leave the rest on default settings. Don't forget to "Burn bootloader"!
 // No Arduino core functions or libraries are used. Use the makefile if 
 // you want to compile without Arduino IDE.
 //
-// 2021 by Stefan Wagner 
-// Project Files (EasyEDA): https://easyeda.com/wagiminator
-// Project Files (Github):  https://github.com/wagiminator
-// License: http://creativecommons.org/licenses/by-sa/3.0/
+// Fuse settings: -U lfuse:w:0x3b:m -U hfuse:w:0xff:m
 
+
+// ===================================================================================
+// Libraries and Definitions
+// ===================================================================================
 
 // Libraries
 #include <avr/io.h>             // for gpio
 #include <avr/sleep.h>          // for the sleep modes
 #include <avr/interrupt.h>      // for the interrupts
-#include <avr/power.h>          // for the power control
 #include <avr/wdt.h>            // for the watchdog timer
 
 // Pin definitions
 #define LED0          PB0       // pin for LED 0
 #define LED1          PB1       // pin for LED 1
 
-// -----------------------------------------------------------------------------
+// ===================================================================================
 // Watchdog Timer Implementation
-// -----------------------------------------------------------------------------
+// ===================================================================================
 
 // Reset watchdog timer
 void resetWatchdog(void) {
@@ -52,9 +65,9 @@ ISR (WDT_vect) {
   wdt_disable();                          // disable watchdog
 }
 
-// -----------------------------------------------------------------------------
+// ===================================================================================
 // Main Function
-// -----------------------------------------------------------------------------
+// ===================================================================================
 
 // Main function
 int main(void) {
@@ -76,11 +89,11 @@ int main(void) {
   PRR    = (1<<PRADC);                    // shut down ADC
   DIDR0  = 0x1F;                          // disable digital intput buffer
   
-  // Main loop
+  // Loop
   while(1) {
     // Fade in/out LED 0
     uint8_t i = 0;
-    set_sleep_mode (SLEEP_MODE_IDLE);
+    set_sleep_mode(SLEEP_MODE_IDLE);
     while(++i) {
       OCR0A = i;
       sleep_mode();
@@ -92,12 +105,12 @@ int main(void) {
     
     // Sleep for 4 seconds
     OCR0A = 0;
-    set_sleep_mode (SLEEP_MODE_PWR_DOWN);
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     resetWatchdog();
     sleep_mode();
     
     // Fade in/out LED 1
-    set_sleep_mode (SLEEP_MODE_IDLE);
+    set_sleep_mode(SLEEP_MODE_IDLE);
     while(++i) {
       OCR0B = i;
       sleep_mode();
@@ -109,11 +122,11 @@ int main(void) {
     
     // Sleep for 4 seconds
     OCR0B = 0;
-    set_sleep_mode (SLEEP_MODE_PWR_DOWN);
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     resetWatchdog();
     sleep_mode(); 
   }
 }
 
 // Timer0 overflow interrupt service routine
-EMPTY_INTERRUPT (TIM0_OVF_vect);    // nothing to be done here, just wake up from sleep
+EMPTY_INTERRUPT(TIM0_OVF_vect);     // nothing to be done here, just wake up from sleep
